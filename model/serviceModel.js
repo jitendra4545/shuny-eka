9a87c38fd0e3a6ca4e0074146819491312b2e6c1
jitendra4545 <jitendra45.js45@gmail.com>
2023-08-05 11:19:21 +0530

    added

--- model/serviceModel.js
+++ model/serviceModel.js
@@ -4,11 +4,11 @@ const { default: mongoose, model } = require("mongoose");
 const ServiceSchema=mongoose.Schema({
 first_name:{type:String,required:true},
 
-father_name:{type:String,required:true},
+father_name:{type:String},
 mobile:{type:Number,required:true},
 address:{type:String,required:true},
 product:{type:String,required:true},
-product_brand_Model:{type:String,required:true},
+product_brand_Model:{type:String},
 pincode:{type:Number,required:true},
 issue_details:{type:String,required:true},
 status:{type:String,default:"pending"}
